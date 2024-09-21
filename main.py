c96d8fd179a556e22f05ca5734b8e13b63ed65d7
clrminded <the_rocket_scientist@icloud.com>
2024-09-20 22:58:32 -0700

    added player.py and added player to screen in main

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
+from player import Player
 from constants import *
 
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+   
+    player = Player(x, y)
+    dt = 0
     while True:
-        pygame.Surface.fill(screen, "black")
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        screen.fill((0, 0, 0))
         dt = clock.tick(60) / 1000
+        player.draw(screen)
+        pygame.display.flip()
+        
 
 
 if __name__ == "__main__":
