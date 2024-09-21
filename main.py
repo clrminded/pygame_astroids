f3b0154f8ca746b6b7462c7805c7904e13fe137f
clrminded <the_rocket_scientist@icloud.com>
2024-09-20 21:46:13 -0700

    fixed fps in main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -17,6 +19,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
