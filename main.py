acab808126183e41f9e2641b254499ab4ca33594
clrminded <the_rocket_scientist@icloud.com>
2024-09-22 17:54:26 -0700

    finished game basics and added ideas

--- main.py
+++ main.py
@@ -6,6 +6,23 @@ from shot import Shot
 from constants import *
 import sys
 
+"""
+Ideas to implement to extend the game
+- Add a scoring system
+- Implement multiple lives and respawning
+- Add an explosion effect for the asteroids
+- Add acceleration to the player movement
+- Make the objects wrap around the screen instead of disappearing
+- Add a background image
+- Create different weapon types
+- Make the asteroids lumpy instead of perfectly round
+- Make the ship have a triangular hit box instead of a circular one
+- Add a shield power-up
+- Add a speed power-up
+- Add bombs that can be dropped
+- add pew-pew sounds when shots are fired
+- add destruction sounds when asteroids are destroyed
+"""
 
 def main():
     pygame.init()
@@ -62,6 +79,6 @@ def main():
 
         dt = clock.tick(60) / 1000
         
-
+        
 if __name__ == "__main__":
     main()
--- shot.py
+++ shot.py
@@ -9,7 +9,6 @@ class Shot(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, SHOT_RADIUS)
         self.velocity = pygame.Vector2(0,1)
-        
 
     def draw(self, screen):
         pygame.draw.circle(screen, 'white', self.position, self.radius, width=2)
